73e19ca8a1938bff7fe4c9c27ee6440fb3f2c8a7
ComputerCrashZer0 <ComputerCrash0@gmail.com>
2022-05-08 15:30:08 -0400

    Renamed the main script from ACE_678.py to bot.py, Added more SFW rules, Added a meetings.txt file, Created a meetings command

---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+# DiscordBot
+<img align="left" alt="Python" src="https://img.shields.io/badge/python%20-%2314354C.svg?&style=for-the-badge&logo=python&logoColor=white"/>
+<img align="left" alt="JSON" src="https://img.shields.io/badge/json%20-%2314354C.svg?&style=for-the-badge&logo=json&logoColor=white"/>
+<br>
+<br>
+
+This repository is a discord bot that I am building for a server I am actively involed in.
+<br>
+<br>
+
+## Getting Started
+---
+Windows
+```
+pip install -r requirements.txt
+``` 
+Linux
+```bash
+pip3 install -r requirements.txt
+``` 
+### Setup
+--- 
+Once you have installed all of the requirements you will need to edit your config file with your discord token.
+```json
+config\config.json
+```
+
+## Contributing
+---
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- bot.py
+++ bot.py
@@ -9,9 +9,6 @@ TOKEN = data['token']
 
 client = commands.Bot(command_prefix="!")
 
-f = open("rules.txt", "r")
-rules = f.readlines()
-
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -21,21 +18,22 @@ async def on_ready():
 
 @client.command(aliases=['rules'])
 async def rule(ctx, *, number):
+    f = open("rules.txt", "r")
+    rules = f.readlines()
+
     await ctx.send(rules[int(number)-1])
 
-@client.command(aliases=['cls'])
+@client.command(aliases=['cls','delete'])
 @commands.has_permissions(manage_messages = True)
 async def clear(ctx, amount=1):
     await ctx.channel.purge(limit = amount)
 
-@client.command(aliases=['info'])
-async def on_message(ctx):
+@client.command(aliases=['meet','meetup'])
+async def meet_ups(ctx):
+    f = open("meetings.txt", "r")
+    meet = f.readlines()
+    for m in meet:
+        await ctx.send(m)
 
-    channel = client.get_channel(965306655678033981)
-    print(f'{client.user} has responded to a message')                                  # This needs to be updated and used for logging
-    msg = 'Hello {0.author.mention}.\n ACE678 is held on the second Wednesday of each month at 6pm.\n Come join us at the Marietta Square Market. \n https://mariettasquaremarket.com/'
-    await channel.send(msg)
 
-
-    
 client.run(TOKEN)
